f3e1bf2678e15d6f5420f0dace084ec281ae4739
larryNgugi <larry.josephgithaka@gmail.com>
2021-08-30 16:40:37 +0300

    working on templates

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+
 def home(request):
 
-    context = {}
+    context = {
+        'heading': 'My First Blog!',
+        'services': ["Coding.", "Eating.", "Sleeping."],
+    }
 
-    return render(request,"home.html", context)
+    return render(request, "home.html", context)
+
+
+def contact(request):
+
+    context = {
+        'heading' : 'Contact Me',
+        'phone': "(+254) 714 389 500",
+        'address': "Nairobi, Kenya",
+        'email': "larry.josephgithaka@gmail.com",
+    }
+
+    return render(request, "contact.html", context)
